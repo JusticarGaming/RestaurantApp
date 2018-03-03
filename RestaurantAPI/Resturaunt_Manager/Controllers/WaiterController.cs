495ec0cf9b452aeba4bfb7373382395c68bb6772
Unknown <combrinck.vicus@gmail.com>
2018-03-03 14:12:22 +0200

    Waiterid being dumb

--- RestaurantAPI/Resturaunt_Manager/Controllers/WaiterController.cs
+++ RestaurantAPI/Resturaunt_Manager/Controllers/WaiterController.cs
@@ -95,7 +95,7 @@ namespace Resturaunt_Manager.Controllers
       {
          using (var db = new RestaurantDatabase())
          {
-            List<Account> accounts = await db.Account.Where(x => x.WaiterId == waiterid).ToListAsync();
+            List<Account> accounts = await db.Account.Where(x => x.Waiterid == waiterid).ToListAsync();
             if (accounts == null) { throw new KeyNotFoundException(); }
             return accounts.ToList();
          }
--- RestaurantAPI/Resturaunt_Manager/Models/Account.cs
+++ RestaurantAPI/Resturaunt_Manager/Models/Account.cs
@@ -10,23 +10,15 @@ namespace Resturaunt_Manager.Models
     {
         [Key]
         public int Id { get; set; }
-        public int WaiterId { get; set; }
-        private double Amount { get; set; }
-        private double Tip { get; set; }
-
-        private double Total { get; set; }
-
+        public int Waiterid { get; set; }
+        public double Amount { get; set; }
+        public double Tip { get; set; }
+        public double Total { get; set; }
         public DateTime Timestamp { get; set; }
-
-        public virtual ICollection<Table> Tables { get; set; }
+        //public virtual ICollection<Table> Tables { get; set; }
         [Required] public virtual Table Table { get; set; }
-
         //public virtual ICollection<Account> Accounts { get; set; }
-
         public virtual ICollection<MenuItem> MenuItems { get; set; }
-
-        public Account() {
-            //rather do the timestamp after creating the new account, afaik this constructor is gonna get called even when it is loading existing items from DB
-        }
+        public Account() { Timestamp = DateTime.Now; }
     }
 }
